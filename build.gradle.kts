447e710bc20479b1084fe3d1e44799d23e17ea50
hao.zhou <hao.zhou@applovin.com>
2023-10-04 13:15:44 -0700

    remove redundant dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("cn.dev33:sa-token-spring-boot3-starter:1.36.0")
