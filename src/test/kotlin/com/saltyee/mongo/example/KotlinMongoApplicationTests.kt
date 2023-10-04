537507a97eace75020681f2bf94d43d3ad7f7ce8
Hao Zhou <hzhou.me@saltyee.com>
2023-10-03 23:36:02 -0700

    init

---
+++ src/test/kotlin/com/saltyee/mongo/example/KotlinMongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.saltyee.mongo.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMongoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
